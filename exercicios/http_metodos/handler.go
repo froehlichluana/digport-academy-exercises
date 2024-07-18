8425bdeeb66c3812a6faabf9898626b818412a19
froehlichluana <froehlichluana@gmail.com>
2024-07-18 13:42:08 -0300

    exercises

--- exercicios/http_metodos/handler.go
+++ exercicios/http_metodos/handler.go
@@ -5,5 +5,16 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+
+	"github.com/froehlichluana/exercicios/http_metodos/pessoa"
 )
 
+func Rotas() *mux.Router {
+	rotas := mux.NewRouter()
+	rotas.HandleFunc("/lista/pessoas", HandlePessoa).Methods("GET")
+	return rotas
+}
+
+func HandlePessoa(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(pessoa.ContactList())
+}
--- exercicios/http_metodos/pessoa/pessoa.go
+++ exercicios/http_metodos/pessoa/pessoa.go
@@ -4,13 +4,13 @@ package pessoa
 type Pessoa struct {
 	Nome string `json:"nome"`
 	Telefone int `json:"telefone"`
-	Email string `json: "email"`
+	Email string `json:"email"`
 }
 
 func ContactList() []Pessoa {
 	pessoas := []Pessoa
-	{Nome:"Julia",Telefone:123456789,Email:"julia@example.com"}
-	{Nome:"Maria",Telefone:987654321,Email:"maria@example.com"}
-	{Nome:"Ana",Telefone:198765432, mail:"ana@example.com"}
+	{Nome:"Camila",Telefone:123456789,Email:"camila@example.com",},
+	{Nome:"Maria",Telefone:987654321,Email:"maria@example.com",},
+	{Nome:"Ana",Telefone:198765432, mail:"ana@example.com",}
 	return pessoas
 }
